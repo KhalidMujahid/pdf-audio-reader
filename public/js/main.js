7cfa63d8ddb0947d76341bacf9bbb66ed860ecf0
khalidMujahid <binkhalid267@gmail.com>
2022-09-24 00:33:35 +0100

    added

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,17 @@ const play = document.querySelector("#play");
 const pause = document.querySelector("#pause");
 
 const utl = new SpeechSynthesisUtterance(text.innerText);
+const instance = new Mark(text);
 
 play?.addEventListener("click", (e) => {
   e.preventDefault();
+  instance.mark(utl.text);
   speechSynthesis.speak(utl);
 });
 
 pause?.addEventListener("click", (e) => {
   e.preventDefault();
   if (speechSynthesis.speaking) {
-    speechSynthesis.pause();
+    speechSynthesis.cancel();
   }
 });
